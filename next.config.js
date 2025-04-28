df8d1ea0ad61f47c7a3821f11e4eb79342ff25d3
Aamir-pathan-ekcs <maakhan@ekcs.co>
2025-04-29 00:32:47 +0530

    1 issue fixed home

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  api: {
-    bodyParser: {
-      sizeLimit: '900mb', // Adjust as needed
-    },
-  },
-};
+const nextConfig = {};
 module.exports = nextConfig;
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   api: {
+//     bodyParser: {
+//       sizeLimit: '900mb', // Adjust as needed
+//     },
+//   },
+// };
+// module.exports = nextConfig;
